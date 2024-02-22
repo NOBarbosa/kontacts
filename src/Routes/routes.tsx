190ea749add015f0a45f065261bc743b07d4a2df
Natalia Barbosa <natalia.barbosa@zig.fun>
2024-02-22 18:17:56 -0300

    feat: routes

---
+++ src/Routes/routes.tsx
@@ -0,0 +1,10 @@
+import { Route, Routes } from "react-router-dom";
+import { Login } from "../Pages/Login/Login";
+
+export function Router() {
+  return (
+    <Routes>
+      <Route path="/login" element={<Login />} />
+    </Routes>
+  );
+}

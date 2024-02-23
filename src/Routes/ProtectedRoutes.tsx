1cf9a00123fc6e9284fb89dd4a07fcf3125d1ef0
Natalia Barbosa <natalia.barbosa@zig.fun>
2024-02-22 21:51:41 -0300

    protected route

--- src/Routes/ProtectedRoutes.tsx
+++ src/Routes/ProtectedRoutes.tsx
@@ -1,7 +1,7 @@
 import { Navigate, Outlet } from "react-router-dom";
 
 export function ProtectedRoute() {
-  const auth = { token: false };
+  const auth = { token: true };
 
   return auth.token ? <Outlet /> : <Navigate to="/login" />;
 }

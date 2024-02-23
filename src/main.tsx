b7a695e4e931ad21fd1039fa016918a6c4c0f210
Natalia Barbosa <natalia.barbosa@zig.fun>
2024-02-22 21:25:39 -0300

    styles

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { Router } from "./Routes/routes.tsx";
+import "./Styles/global.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
